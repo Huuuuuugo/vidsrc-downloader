d5260c1e5f84f88ccbf105ffaf75990a36a69649
Hugo Messias <hugomessias9@gmail.com>
2024-09-30 16:50:23 -0300

    Updated to automatically interact with the page to get the target request

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import re
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import seleniumwire.undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 options = Options()
@@ -37,4 +40,29 @@ driver.request_interceptor = interceptor
 
 driver.get("https://vidsrc.net/embed/tv?imdb=tt3559912&season=1&episode=1")
 
+# wait for obstructing element to appear and then remove it
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "dontfoid"))
+)
+driver.execute_script("document.getElementById('dontfoid').remove();")
+
+# switch to player iframe
+iframe = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "player_iframe"))
+)
+driver.switch_to.frame(iframe)
+
+# get title of the video
+title = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "iframe_title"))
+)
+title = title.text
+
+# click the play button
+play_btn = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "pl_but"))
+)
+play_btn.click()
+
+print(title)
 input("Press Enter to exit")
